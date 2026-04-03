0d5b69c56e3ee20f21b2f5a3c54cfe781f3877cb
Kim Jeongmin <131476493+wjdals333@users.noreply.github.com>
2026-04-03 15:04:21 +0900

    Update 2-4.c

---
+++ code/2-4.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+long my_strtol(char *str, char **endstr) {
+    long res = 0;
+
+    while (*str >= '0' && *str <= '9') {
+        res = res*10 + (*str - '0');
+
+        str++;
+    }
+    *endstr = str;
+  
+    return res;
+}
+
+int main() {
+    char str[] = "2026hello";
+    char *end;
+
+    long val = my_strtol(str, &end);
+
+    printf("%ld %s\n", val, end);
+
+    return 0;
+}
